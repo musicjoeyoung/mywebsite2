97c92742ba10bc7c64ee1b7ce58e85c33d8cfce4
Joe Young <musicjoeyoung@gmail.com>
2021-04-16 13:27:46 -0400

    error update

--- src/ErrorNotFound.js
+++ src/ErrorNotFound.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
-import img from "./iconfinder_cat_hungry_185523.png";
+//import img from "./iconfinder_cat_hungry_185523.png";
 
 export class ErrorNotFound extends Component {
   render() {
     return (
       <div>
-        <img src={img} alt="404" id="cat404" />
-        <h2>404</h2>
-        <h4>
-          We're sorry the page you were looking for doesn't exist. <br />
-          This page is empty... <br />
-          <br />
-          As an apology...
-          <br />I will let you hire me.
-        </h4>
+        <h1>404</h1>
+        <p>
+          I'm just as confused as you are. <br />
+          This page you were looking for doesn't exist. <br />
+        </p>
+        But also... <br />
+        <h2>
+          var chickenOrEgg = [🥚, 🐔] <br />
+          chickenOrEgg.sort() <br />
+          result: [🐔,🥚]
+        </h2>
+        You're welcome.
+        <br />
+        As an apology...
+        <br />I will let you hire me.
         <a href="/mainpage">
           <h2 id="enter">Come back home!</h2>
         </a>
